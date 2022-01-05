fb01aa57cbf9bfced08c03f70e6cd190cee6e82d
root <root@hiroki-thinkpad.localdomain>
2022-01-05 15:16:16 +0900

    fix: directory structure

---
+++ FunctionAppWithSQLServerTests/Repository/SQLServerRowRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.SqlClient;
+
+namespace FunctionAppWithSQLServer.Repository.Tests
+{
+    [TestClass()]
+    public class SQLServerRowRepositoryTests
+    {
+        [TestMethod()]
+        public void GetValueTest()
+        {
+            #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            string connectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
+            #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                RowRepository repo = SQLServerRowRepository.OfConnection(conn);
+                Assert.AreEqual(repo.GetFirstValue("key"), "value");
+            }
+        }
+    }
+}
